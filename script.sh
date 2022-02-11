cfbf15ae2b5d51038681055cb7e88f53efd4233d
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2022-02-11 22:34:54 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,5 +6,5 @@ JOKE=$(curl https://joke-of-the-day-api.herokuapp.com/joke-of-the-day)
 QUESTION=$(jq .question <<< $JOKE | sed 's/"//g')
 ANSWER=$(jq .answer <<< $JOKE | sed 's/"//g')
 
-perl -i'' -pe "s/(?<=<\!-- script:start TODAY --> ).+(?= <\!-- script:end TODAY -->)/$DATE/g" README.md
+perl -i'' -pe "s/(?<=<\!-- script:start TODAY --> ).+(?= <\!-- script:end TODAY -->)/($DATE)/g" README.md
 perl -i'' -0pe "s/(?<=<!-- script:start JOKE -->)[\w\W\s]*(?=<!-- script:end JOKE -->)/\n**$QUESTION**\n\n*$ANSWER*\n/ms" README.md
